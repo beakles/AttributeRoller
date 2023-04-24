5cf1810d5b9098016db5fea789ea3d9d9f453ddd
Brannon Eakles <beakles@ucsc.edu>
2023-04-24 05:16:01 -0700

    Updating code

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ class Player {
       wisdom: 0,
       charisma: 0
     };
-    
+
     let shuffledResult = shuffleArray(defaultAttributeScores);
     for (const [key, value] of Object.entries(this.attributes)) {
       let attributeValue = shuffledResult.pop();
@@ -54,4 +54,13 @@ function shuffleArray(targetArray) {
   }
   
   return shuffled;
+}
+
+function diceRoller(times, sides) {
+  let results = [];
+  for (let i = 0; i < times; i++) {
+    results.push(Math.floor(math.random() * sides + 1));
+  }
+
+  return results;
 }
